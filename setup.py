08107bdaed25689f49c830c7d8fee9d7c3e895fc
deepjyoti30 <deep.barman30@gmail.com>
2018-11-08 13:56:39 +0530

    First release

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 if __name__ == '__main__':
     setuptools.setup(
         name="ytmdl",
-        version="0.3-r5",
+        version="2018.11.08",
         author="Deepjyoti Barman",
         author_email="deep.barma30@gmail.com",
         description="Youtube Music Downloader",
