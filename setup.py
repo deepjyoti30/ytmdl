9882bb51b0e191fa224c821f44a2da666888ffa0
deepjyoti30 <deep.barman30@gmail.com>
2019-04-23 03:26:57 +0530

    Temp bug fix

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 if __name__ == '__main__':
     setuptools.setup(
         name="ytmdl",
-        version="2019.1.30.1",
+        version="2019.4.23",
         author="Deepjyoti Barman",
         author_email="deep.barma30@gmail.com",
         description="Youtube Music Downloader",
@@ -24,11 +24,11 @@ if __name__ == '__main__':
         ),
         scripts=['bin/ytmdl'],
         install_requires=[
-            'youtube_dl==2019.01.30.1',
-            'mutagen==1.40.0',
-            'itunespy==1.5.5',
-            'requests==2.20.0',
-            'colorama==0.3.9',
-            'bs4==0.0.1'
+            'youtube_dl',
+            'mutagen',
+            'itunespy',
+            'requests',
+            'colorama',
+            'bs4'
         ]
     )
