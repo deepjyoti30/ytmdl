c8b60a1aa8acd54664cf7988d6df24e88e763cd6
deepjyoti30 <deep.barman30@gmail.com>
2019-04-23 19:46:28 +0530

    Version changes

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 if __name__ == '__main__':
     setuptools.setup(
         name="ytmdl",
-        version="2019.4.23",
+        version="2019.4.23.2",
         author="Deepjyoti Barman",
         author_email="deep.barma30@gmail.com",
         description="Youtube Music Downloader",
