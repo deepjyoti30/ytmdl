0ee836d7742e950a202c1f2f8035ebfd1a43497f
deepjyoti30 <deep.barman30@gmail.com>
2019-11-16 13:04:29 +0530

    update: Added python_requires in setup.py.

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ if __name__ == '__main__':
             "License :: OSI Approved :: MIT License",
             "Operating System :: OS Independent",
         ),
+        python_requires=">=3.*",
         scripts=['bin/ytmdl'],
         install_requires=req_pkgs,
         setup_requires=req_pkgs
