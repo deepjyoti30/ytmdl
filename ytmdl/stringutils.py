83f02fe528de1b369aa0f5ced7f9f98f00be8815
deepjyoti30 <deep.barman30@gmail.com>
2020-04-05 13:33:59 +0530

    update: Playlist implemented.

--- ytmdl/stringutils.py
+++ ytmdl/stringutils.py
@@ -93,7 +93,7 @@ def remove_yt_words(title):
     """
     Remove words like Official video etc from the name of the song
     """
-    title = re.sub(r'\]|\[|official|video|-', '', str(title).lower())
+    title = re.sub(r'\]|\[|official|video|music|-|\)|\(|®', '', str(title).lower())
     # Replace more than one space with one space
     title = re.sub(r'\ {2}', ' ', title)
     return title
