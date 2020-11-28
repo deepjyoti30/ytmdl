b312503f45275e2c1f94ec87e9e6dab7ef9aaaba
Deepjyoti Barman <deep.barman30@gmail.com>
2020-11-28 17:27:29 +0530

    Add conversion Error in the exceptions module

--- ytmdl/exceptions.py
+++ ytmdl/exceptions.py
@@ -22,3 +22,22 @@ class DownloadError(Exception):
 
     def __str__(self) -> str:
         return self.__message
+
+
+class ConvertError(Exception):
+    """Exception for conversion erros.
+
+    This exception is raised whenever the conversion goes wrong,
+    mostly while using ffmpeg or something similar.
+    """
+    def __init__(self, error) -> None:
+        super().__init__()
+
+        self.__message = self.__build_message(error)
+
+    def __build_message(self, error) -> str:
+        """Build the error message"""
+        return "Conversion failed with error: {}".format(error)
+
+    def __str__(self) -> str:
+        return self.__message
