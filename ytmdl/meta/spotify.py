cfb7b2aad9943eee0f09cb0dc0ea3732cfb07e2d
Deepjyoti Barman <deep.barman30@gmail.com>
2021-07-26 16:48:33 +0530

    Add init file to add support for Spotify metadata

---
+++ ytmdl/meta/spotify.py
@@ -0,0 +1,10 @@
+"""
+Handle extracting metadata from Spotify
+
+We will use Spotify's API to search for tracks and show them
+to the user to be used accordingly.
+"""
+
+
+CLIENT_ID = "a166f23a5637429e8bd819df46fa034e"
+CLIENT_SECRET = "bbad17df3cde471ab969f081f2ea8bbb"
