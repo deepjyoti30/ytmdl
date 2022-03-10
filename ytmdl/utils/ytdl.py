2ddb6f30dd5ec79c981982dad6e4c154f5d05c9d
Deepjyoti Barman <deep.barman30@gmail.com>
2022-03-10 21:06:21 +0530

    Add a method to get ydl_opts with basic details

--- ytmdl/utils/ytdl.py
+++ ytmdl/utils/ytdl.py
@@ -4,6 +4,10 @@ Handle ytdl related util methods.
 
 from os import path
 from simber import Logger
+from typing import Dict
+from yt_dlp import parse_options
+
+from ytmdl import utility
 
 
 # Create logger
@@ -27,3 +31,31 @@ def is_ytdl_config_present(path_passed: str) -> bool:
     logger.debug("Checking if path exists: ", path_passed)
 
     return path.exists(path_passed)
+
+
+def ydl_opts_with_config(ytdl_config: str = None) -> Dict:
+    """
+    Generate the ydl_opts dictionary based on the passed config
+    path.
+
+    If the config is not present, return an empty dictionary
+    """
+    is_quiet: bool = utility.determine_logger_level(
+    ) != logger.level_map["DEBUG"]
+    no_warnings: bool = utility.determine_logger_level(
+    ) > logger.level_map["WARNING"]
+
+    ydl_opts = {
+        "quiet": is_quiet,
+        'no_warnings': no_warnings,
+        'nocheckcertificate': True,
+        'source_address': '0.0.0.0'
+    }
+
+    # If config is passed, generated opts with config
+    if ytdl_config is not None:
+        _, _, _, parsed_opts = parse_options(
+            f"yt-dlp --config-locations {ytdl_config}")
+        ydl_opts.update(parse_options)
+
+    return ydl_opts
