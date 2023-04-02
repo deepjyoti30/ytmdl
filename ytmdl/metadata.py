a16939170c67248be9c7ce2a046c6b719ec07476
ArchemedIan <105951210+ArchemedIan@users.noreply.github.com>
2023-04-02 12:39:21 -0400

    maybe fix ampersand, single/double quotes in lookups

--- ytmdl/metadata.py
+++ ytmdl/metadata.py
@@ -148,9 +148,11 @@ def _search_tokens(song_name, song_list):
         name = song.track_name.lower()
         # If there is a part like (featuring ..) or any extra data
         # we should remove it as it doesn't aid the search
-        name = re.sub(r'\([^)]*\)', '', name)
+        name = re.sub('"', '', name)   #remove double quote mark w/o regex because it messes with remove_punct regex
+        name = remove_punct(name)       #remove sipngle quote mark before regexs because titles have apostrophes sometimes
+        name = re.sub(r'\([^]*\)', '', name) #Removed orpan parentesis 
+        name = re.sub(r'&', 'and', name) #replace ampersands with and to avoid api issues
         name = remove_stopwords(name)
-        name = remove_punct(name)
         name = remove_multiple_spaces(name)
         name = unidecode(name)
         tokens2 = name.split()
