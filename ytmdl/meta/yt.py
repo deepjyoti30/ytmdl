b5fdf4669a47b6258203104de7806723b59da4a5
Deepjyoti Barman <deep.barman30@gmail.com>
2023-11-20 18:00:12 +0530

    Add support to parse the details from yt

--- ytmdl/meta/yt.py
+++ ytmdl/meta/yt.py
@@ -2,7 +2,8 @@
 Handle metadata extraction from youtube
 """
 
-from typing import Dict
+from typing import Dict, List
+from datetime import datetime
 
 from yt_dlp import YoutubeDL
 from simber import Logger
@@ -15,12 +16,26 @@ from ytmdl.exceptions import ExtractError
 logger = Logger("meta:yt")
 
 
+def __parse_release_date_from_utc(timestamp: int) -> str:
+    if timestamp is None:
+        return None
+    
+    dt_object = datetime.utcfromtimestamp(timestamp)
+    return dt_object.strftime('%Y-%m-%dT%H:%M:%SZ')
+
+def __parse_genre_name_from_categories(categories: List[str]) -> str:
+    return categories[0] if len(categories) else "N/A"
+
 def __parse_meta_from_details(details: Dict) -> Meta:
     """
     Parse the meta object from the passed details
     """
     return Meta(
-        
+        release_date=__parse_release_date_from_utc(details.get("release_timestamp", None)),
+        track_name=details.get("title", "N/A"),
+        artist_name=details.get("channel", "N/A"),
+        primary_genre_name=__parse_genre_name_from_categories(details.get("categories", [])),
+        artwork_url_100=details.get("thumbnail", "N/A")
     )
 
 def extract_meta_from_yt(video_id: str) -> Meta:
@@ -34,6 +49,7 @@ def extract_meta_from_yt(video_id: str) -> Meta:
     
     try:
         details = ytdl_obj.extract_info(video_url, download=False)
+        return __parse_meta_from_details(details)
     except Exception as e:
         logger.debug("Got exception while extracting details for video with ID: ", video_id)
         logger.warning("Failed to extract metadata from yt with exception: ", str(e))
